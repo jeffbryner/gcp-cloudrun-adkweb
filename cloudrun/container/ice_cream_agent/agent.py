2aea22f9ae16e76c702fd4119e343a8aa68b395c
Jeff Bryner <jeff@jeffbryner.com>
2025-06-29 12:25:49 -0700

    multi agents working together

--- cloudrun/container/ice_cream_agent/agent.py
+++ cloudrun/container/ice_cream_agent/agent.py
@@ -1,6 +1,7 @@
 from google.adk.agents import Agent
 import os
 import google.auth
+from toppings_agent.agent import root_agent as toppings_agent
 
 credentials, project_id = google.auth.default()
 os.environ["GOOGLE_CLOUD_PROJECT"] = project_id
@@ -8,6 +9,7 @@ os.environ["GOOGLE_CLOUD_LOCATION"] = "us-central1"  # change to suitable locati
 
 root_agent = Agent(
     model="gemini-2.5-flash",
-    name="ai_assistant",
-    instruction="you are a friendly agent who loves to talk about ice cream.",
+    name="ice_cream_assistant",
+    instruction="you are a friendly agent who loves to talk about ice cream. You don't know much about toppings, but you can always ask for help from the toppings agent.",
+    sub_agents=[toppings_agent],
 )
